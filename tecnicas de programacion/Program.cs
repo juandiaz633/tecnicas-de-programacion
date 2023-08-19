7e1468dc822c5c23742d8ffb8a5fd6532ff720ff
Juan Diaz Castillo <juan.diazc@konradlorenz.edu.co>
2023-08-19 11:30:02 -0500

    Add comment

--- tecnicas de programacion/Program.cs
+++ tecnicas de programacion/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //comment
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("the index is" + i);
+            }
         }
     }
 }
