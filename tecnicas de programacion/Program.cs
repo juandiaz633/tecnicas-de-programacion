ca884b311c05251fb16fdfcecc550dcfd2025be5
usuario <usuario@EDI104EST06>
2023-08-19 11:14:24 -0500

    Agregar archivos de proyecto.

---
+++ tecnicas de programacion/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace tecnicas_de_programacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
