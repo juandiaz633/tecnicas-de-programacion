da0f0789acc61c6dd9c91297253bf5614a43e675
juandiaz633 <142682193+juandiaz633@users.noreply.github.com>
2023-08-26 13:11:02 -0500

    Update Program.cs

--- tecnicas de programacion/Program.cs
+++ tecnicas de programacion/Program.cs
@@ -37,6 +37,111 @@ namespace tecnicas_de_programacion
             Console.WriteLine("El numero ingresado anteriormente es:" + (number + 1));
 
 
+
+            //Ejercicio 14-----------------------------------------------------------------------------------------------------
+using System;
+
+Console.WriteLine("Algoritmo para calcular el volumen del cilindro y saber si es positivo");
+Console.Write("Añade los valores del radio");
+
+Console.WriteLine("Algoritmo para calcular el volumen del cilindro");
+Console.Write("Añade los valores del radio");
+double radio = Convert.ToDouble(Console.ReadLine());
+
+
+    Console.Write("Añade los valores de la altura");
+double altura = Convert.ToDouble(Console.ReadLine());
+double pi = Math.PI;
+
+double volumen = Convert.ToDouble(radio * altura * pi);
+
+if (true)
+{
+
+}
+else {
+
+    volumen > = 12;
+
+}
+
+
+Console.WriteLine("El volumen es:" + Math.Round(volumen, 3));
+
+
+
+
+//Ejercicio 13------------------------------------------------------------------------------------------------
+
+Console.WriteLine("Algoritmo para transformar temperatura Fahrenheit a Celsuius");
+Console.Write("Añade la temperatura Fahrenheit");
+
+double fh = Convert.ToDouble(Console.ReadLine());
+
+double conversionfh = Convert.ToDouble((fh - 32) * 5 / 9);
+
+Console.WriteLine("El grado Fahrenheit convertido a grados Celsuius es: " + conversionfh);
+
+
+//Ejercicio #12------------------------------------------------------------------------------------------------
+
+Console.WriteLine("Algoritmo para transformar centimentros a yardas, metros, pies y pulgadas");
+Console.Write("añade los centimetros que quieres convertir");
+
+double cm = Convert.ToDouble(Console.ReadLine());
+
+double conversionyd = Convert.ToDouble(cm / 91.44);
+
+double conversionmt = Convert.ToDouble(cm / 100);
+
+double conversionps = Convert.ToDouble(cm / 30.48);
+
+double conversionpg = Convert.ToDouble(cm / 2.54);
+
+Console.WriteLine("El centimetro convertido es " +  conversionyd,conversionmt,conversionps, conversionpg );
+
+Console.WriteLine("Yardas: " + conversionyd);
+
+Console.WriteLine("Metros: " + conversionmt);
+
+Console.WriteLine("Pies: " + conversionps);
+
+Console.WriteLine("Pulgadas: " + conversionpg);
+
+//Ejercicio #11--------------------------------------------------------------------------------------------------------------------------
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Algoritmo para calcular el volumen del cilindro");
+            Console.Write("Añade los valores del radio");
+            double radio = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Añade los valores de la altura");
+            double altura = Convert.ToDouble(Console.ReadLine());
+            double pi = Math.PI;
+
+            double volumen = Convert.ToDouble(radio * altura * pi);
+
+
+            Console.WriteLine ("El volumen es:" + Math.Round(volumen, 3));
+
+            
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
  }
     }
     
